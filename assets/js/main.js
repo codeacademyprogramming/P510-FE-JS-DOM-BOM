72ade7432f1ededc99d0b0e639c7d52477700008
agilatakishiyev <agil.atakishiyev@pashabank.az>
2021-05-15 20:28:58 +0400

    added files

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// DOM - Document Object Model
+// console.log(window.document);
+
+// BOM - Browser Object Model
+// console.log(window.location);
+
+
+// const linkToSimbrella = document.querySelector('a');
+// const links = Array.from(document.querySelectorAll('a'));
+// console.log(links);
+
+// links.forEach((link) => {
+//     link.style.color = 'red';
+// });
+
+// links.item(0).style.backgroundColor = 'orange';
+
+// links[1].style.backgroundColor = 'black';
+
+// NodeList is not an array type, it is array like
+// links.
+
+// const linksWithTagnames = Array.from(document.getElementsByTagName('a'));
+// const linksWithClasses = Array.from(document.getElementsByClassName('link'));
+// const linkWithId = document.getElementById('simbrella');
+
+
+// console.log(linkWithId);
+
+
+// linksWithTagnames.forEach(link => {
+//     link.style.color = 'red';
+// })
+
+// const countryNames = ['Azerbaijan', 'Turkey', 'Georgia', 'Israel', 'USA'];
+// const unOrderedList = document.getElementById("list-id");
+
+// countryNames.forEach((countryName) => {
+//     const listItem = document.createElement('li'); // <li></li>
+//     listItem.innerText = countryName; // <li>Azerbaijan</li>
+//     unOrderedList.appendChild('salam dunya');
+// });
+
